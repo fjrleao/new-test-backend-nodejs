b927a9f9465eb7f968b8ac3e9dee3bdaecd7f4d6
Fábio Júnior <fjrleao@gmail.com>
2023-08-27 20:16:20 -0300

    feat: sqs-consumer proccess queue messages

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
 import app from './app'
 import 'dotenv/config'
 import { DatabaseConfig } from './configs/database'
+import { Consumer } from 'sqs-consumer'
+import { CatalogService } from './services/catalog.service'
 
 const PORT = process.env.PORT || 3000
 
 DatabaseConfig.connect().then(() => {
 	app.listen(PORT, () => {
 		console.log(`Server is running in port ${PORT}`)
+		const consumer = Consumer.create({
+			queueUrl: process.env.AWS_SQS_URL,
+			handleMessage: async (message) => {
+				console.log(JSON.parse(message.Body).owner)
+				const catalogService = new CatalogService(DatabaseConfig.getDatabase())
+				await catalogService.generateCatalogAndUploadToS3(
+					JSON.parse(message.Body).owner
+				)
+			},
+		})
+		consumer.on('error', (err) => {
+			console.log(err)
+		})
+		consumer.on('processing_error', (err) => {
+			console.error(err.message)
+		})
+
+		consumer.start()
 	})
 })
