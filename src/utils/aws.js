5cfdcb47e2625734e404db0182ca5add92033262
Fábio Júnior <fjrleao@gmail.com>
2023-08-27 15:28:25 -0300

    refactor: create s3 instance in constuctor

--- src/utils/aws.js
+++ src/utils/aws.js
@@ -1,14 +1,18 @@
 import { S3 } from 'aws-sdk'
 import 'dotenv/config'
 
-export class AWS {
-	async uploadS3JSONFile(fileName, fileContent) {
-		const s3 = new S3({
+export class AWSS3 {
+	#s3
+
+	constructor() {
+		this.#s3 = new S3({
 			apiVersion: '2006-03-01',
 			region: process.env.AWS_REGION,
 		})
+	}
 
-		await s3
+	async uploadS3JSONFile(fileName, fileContent) {
+		await this.#s3
 			.upload({
 				Bucket: process.env.AWS_S3_BUCKET,
 				Key: fileName,
